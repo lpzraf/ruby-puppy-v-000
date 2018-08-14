7f79581266dbaa512f99b4e6f50cb04aeea25a0e
Rafael Lopez <lpz.raf@gmail.com>
2018-08-14 23:42:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,19 @@ class Dog
  
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all.each do |dogs_name|
+      puts dogs_name.name
+    end
+  end
+  
+  def self.clear_all
+    @@all.each do |clear|
+      @@all.clear
+    end
   end
 end
  
